79351712c4e2ace881bbd2058abef4b00e87938b
z5376268 <z5376268>
2023-02-13 02:30:16 +1100

    use trackingmore API to get using tracking number

--- components/History.js
+++ components/History.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-native/no-inline-styles */
 import zIndex from '@mui/material/styles/zIndex';
-import { borderRadius, fontFamily, margin } from '@mui/system';
+import {borderRadius, fontFamily, margin} from '@mui/system';
 import React, {useState} from 'react';
 import {
   View,
@@ -12,9 +12,10 @@ import {
   Animated,
   TextInput,
   TouchableWithoutFeedback,
+  RefreshControl,
+  ScrollView,
 } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
 
 const DATA = [
   {
@@ -26,7 +27,8 @@ const DATA = [
     location: 'Toronto, ON, CA',
     progress: 0.6,
     image: require('./images/sampleNike.png'),
-    latitude: 37.78825, longitude: -122.4324
+    latitude: 37.78825,
+    longitude: -122.4324,
   },
   {
     key: '2',
@@ -37,7 +39,8 @@ const DATA = [
     location: 'Toronto, ON, CA',
     progress: 1,
     image: require('./images/sampleLV.png'),
-    latitude: 57, longitude: 2
+    latitude: 57,
+    longitude: 2,
   },
   {
     key: '3',
@@ -48,7 +51,8 @@ const DATA = [
     location: 'Toronto, ON, CA',
     progress: 0.7,
     image: require('./images/sampleAdidas.png'),
-    latitude: 36.2048, longitude: 138.2529
+    latitude: 36.2048,
+    longitude: 138.2529,
   },
   {
     key: '4',
@@ -59,7 +63,8 @@ const DATA = [
     location: 'Ottawa, ON, CA',
     progress: 0.6,
     image: require('./images/sampleJordan.png'),
-    latitude: 47, longitude: 8
+    latitude: 47,
+    longitude: 8,
   },
   {
     key: '5',
@@ -70,7 +75,8 @@ const DATA = [
     location: 'Toronto, ON, CA',
     progress: 0.2,
     image: require('./images/sampleLV.png'),
-    latitude: 41, longitude: 12
+    latitude: 41,
+    longitude: 12,
   },
   {
     key: '6',
@@ -81,7 +87,8 @@ const DATA = [
     location: 'Ottawa, ON, CA',
     progress: 0.1,
     image: require('./images/sampleNike.png'),
-    latitude: 34, longitude: 151
+    latitude: 34,
+    longitude: 151,
   },
   {
     key: '7',
@@ -92,113 +99,184 @@ const DATA = [
     location: 'Toronto, ON, CA',
     progress: 0.5,
     image: require('./images/sampleAdidas.png'),
-    latitude: 90.78825, longitude: 122.4324
+    latitude: 90.78825,
+    longitude: 122.4324,
+  },
+  {
+    key: '8',
+    brand: 'Adidas',
+    product: 'Ultraboost 5.0',
+    last_update: '12/10/22',
+    status: 'In Transit',
+    location: 'Toronto, ON, CA',
+    progress: 0.5,
+    image: require('./images/sampleAdidas.png'),
+    latitude: 90.78825,
+    longitude: 122.4324,
   },
 ];
+let dataIds = [];
 
 function HistoryScreen({navigation}) {
   const [formPosition] = useState(new Animated.Value(-100));
   const [isFormVisible, setIsFormVisible] = useState(false);
   const [overlayOpacity] = useState(new Animated.Value(0));
   const [formData, setFormData] = useState({});
+  const [refreshing, setRefreshing] = useState(false);
 
-  const handleSubmit = () => {
+  const onRefresh = () => {
+    setRefreshing(true);
+    // Perform data fetching or other actions to refresh the list
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000); // Example: simulate a 2 second delay for data fetching
+  };
+
+  const handleSubmit = async () => {
     //console.log(formData.orderNum);
     const options = {
       method: 'POST',
-      headers: {'Content-Type': 'application/json', 'Tracking-Api-Key': 'httpw3s0-mw3x-2295-24sj-3at7gkx22eh3 '},
-      body: '{"tracking_number":"9261290312833844954982"}'
+      headers: {
+        'Content-Type': 'application/json',
+        'Tracking-Api-Key': 'uyhewhih-yoj8-3k4p-ejz5-eh84roy26ffw',
+      },
+      body: `{"tracking_number":"${formData.orderNum}"}`,
     };
-    
-    fetch('https://api.trackingmore.com/v4/couriers/detect', options)
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
-  
-    fetch(`https://api.trackingmore.com/v4/trackings/get?tracking_number=${formData.orderNum}&carrier_code=dachser`, {
-        method: 'GET',
-        headers: {
+
+    try {
+      const response = await fetch(
+        'https://api.trackingmore.com/v4/couriers/detect',
+        options,
+      );
+      const result = await response.json();
+
+      const detectedCourier = result.data;
+      console.log(detectedCourier);
+      const courierCodes = detectedCourier.map(courier => courier.courier_code);
+      console.log(courierCodes);
+      const promises = courierCodes.map(courierCode =>
+        fetch('https://api.trackingmore.com/v4/trackings/create', {
+          method: 'POST',
+          headers: {
             'Content-Type': 'application/json',
-            'Trackingmore-Api-Key': 'tvb39aav-uk2u-xsqj-0ps7-z8s48r5q830b'
-        }
-    })
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      const filteredData = data.data.items.filter(item => item.tracking_number === formData.orderNum);
-      console.log(filteredData);
-      for (let i = 0; i < filteredData.length; i++) {
-        console.log(filteredData[i]["origin_info"]);
-      }
-    })
-  
-    .catch(error => console.error('Error:', error));
+            'Tracking-Api-Key': 'uyhewhih-yoj8-3k4p-ejz5-eh84roy26ffw',
+          },
+          body: `{"tracking_number":"${formData.orderNum}","courier_code":"${courierCode}"}`,
+        }).then(response => response.json()),
+      );
+      dataIds.push(formData.orderNum);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+    try {
+      const promises = dataIds.map(trackingnum =>
+        fetch(
+          `https://api.trackingmore.com/v4/trackings/get?tracking_numbers=${trackingnum}`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Tracking-Api-Key': 'uyhewhih-yoj8-3k4p-ejz5-eh84roy26ffw',
+            },
+          },
+        )
+          .then(response => response.json())
+          .then(response =>
+            response.data.filter(item => item.substatus !== 'notfound002'),
+          )
+          .catch(err => console.error(err)),
+      );
+
+      const newData = await Promise.all(promises);
+      console.log(newData);
+      let i = 0;
+      newData.forEach(tracking => {
+        console.log(i);
+        console.log(tracking[0].origin_info);
+        i++;
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
     setIsFormVisible(false);
     setFormData({});
     Animated.timing(formPosition, {
-        toValue: -200,
-        duration: 500,
-        useNativeDriver: true,
+      toValue: -200,
+      duration: 500,
+      useNativeDriver: true,
     }).start();
     Animated.timing(overlayOpacity, {
-        toValue: 0,
-        duration: 500,
-        useNativeDriver: true,
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
     }).start();
-  }
-
-
+  };
 
   return (
-    <TouchableWithoutFeedback onPress={() => {
-      setIsFormVisible(false);
-      Animated.timing(formPosition, {
+    <TouchableWithoutFeedback
+      onPress={() => {
+        setIsFormVisible(false);
+        Animated.timing(formPosition, {
           toValue: -200,
           duration: 500,
           useNativeDriver: true,
-      }).start();
-      Animated.timing(overlayOpacity, {
+        }).start();
+        Animated.timing(overlayOpacity, {
           toValue: 0,
           duration: 500,
           useNativeDriver: true,
-      }).start();
-    }}>
+        }).start();
+      }}>
       <View style={{flex: 1}}>
-        <Animated.View style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, backgroundColor: 'rgba(0, 0, 0, 0.5)', opacity: overlayOpacity }}></Animated.View>
+        <Animated.View
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            opacity: overlayOpacity,
+          }}></Animated.View>
         <FlatList
           contentContainerStyle={styles.listContainer}
           data={DATA}
           showsHorizontalScrollIndicator={false}
           showsVerticalScrollIndicator={false}
-          ListHeaderComponent={() => 
+          ListHeaderComponent={() => (
             <View>
               <View style={styles.header}>
                 <Text style={styles.titleText}>Orders</Text>
                 <TouchableOpacity
-                style={styles.headButton}
-                onPress={() => {
-                  setIsFormVisible(!isFormVisible);
-                  Animated.timing(formPosition, {
-                    toValue: isFormVisible ? -200 : 0,
-                    duration: 500,
-                    useNativeDriver: true,
-                  }).start();
-                  Animated.timing(overlayOpacity, {
-                    toValue: isFormVisible ? 0 : 1,
-                    duration: 500,
-                    useNativeDriver: true,
-                  }).start();
-                }}>
-                  <MaterialCommunityIcons name="plus" color={'#fff'} size={25} />
+                  style={styles.headButton}
+                  onPress={() => {
+                    setIsFormVisible(!isFormVisible);
+                    Animated.timing(formPosition, {
+                      toValue: isFormVisible ? -200 : 0,
+                      duration: 500,
+                      useNativeDriver: true,
+                    }).start();
+                    Animated.timing(overlayOpacity, {
+                      toValue: isFormVisible ? 0 : 1,
+                      duration: 500,
+                      useNativeDriver: true,
+                    }).start();
+                  }}>
+                  <MaterialCommunityIcons
+                    name="plus"
+                    color={'#fff'}
+                    size={25}
+                  />
                 </TouchableOpacity>
               </View>
             </View>
-          }
+          )}
           // ListFooterComponent={() => <View style={styles.separator} />}
           renderItem={({item}) => (
             <View style={styles.itemContainer}>
-              <TouchableOpacity 
-                onPress={() => navigation.navigate('MapTrack', {item})}>
+              <TouchableOpacity
+                onPress={() => navigation.navigate('MapTrack', {item})}
+                style={{flexDirection: 'row'}}>
                 <View style={styles.item}>
                   <Image
                     style={{width: '100%', height: '100%'}}
@@ -206,54 +284,61 @@ function HistoryScreen({navigation}) {
                     resizeMode="contain"
                   />
                 </View>
-              </TouchableOpacity>
-              <View style={styles.textContainer}>
-                <Text style={styles.orderText}>
-                  {item.brand}, {item.product}
-                </Text>
-                <Text style={styles.orderText}>
-                  {item.last_update}, {item.status}
-                </Text>
-                <View style={styles.progressBar}>
-                  <View
-                    style={{
-                      flex: item.progress,
-                      backgroundColor: 'black',
-                      height: 3,
-                    }}
-                  />
-                  <View
-                    style={{
-                      flex: 1 - item.progress,
-                      backgroundColor: 'black',
-                      height: 1,
-                      marginRight: 10,
-                    }}
-                  />
+                <View style={styles.textContainer}>
+                  <Text style={styles.orderText}>
+                    {item.brand}, {item.product}
+                  </Text>
+                  <Text style={styles.orderText}>
+                    {item.last_update}, {item.status}
+                  </Text>
+                  <View style={styles.progressBar}>
+                    <View
+                      style={{
+                        flex: item.progress,
+                        backgroundColor: 'black',
+                        height: 3,
+                      }}
+                    />
+                    <View
+                      style={{
+                        flex: 1 - item.progress,
+                        backgroundColor: 'black',
+                        height: 1,
+                        marginRight: 10,
+                      }}
+                    />
+                  </View>
+                  <Text style={styles.orderText}>{item.location}</Text>
                 </View>
-                <Text style={styles.orderText}>{item.location}</Text>
-              </View>
+              </TouchableOpacity>
             </View>
           )}
         />
-        <Animated.View style={[styles.formContainer, { transform: [{translateY: formPosition}],
-          display: isFormVisible ? 'flex' : 'none'}]}>
-        <Text style={styles.inputText}>Insert Package/Order Number</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Order Number"
-          value={formData.orderNum || ''}
-          onChangeText={text => setFormData({ ...formData, orderNum: text })}
-          onSubmitEditing={handleSubmit}
-          returnKeyType={'done'}
-          blurOnSubmit={true}
-        />
-        <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
-          <Text style={styles.submitButtonText}>Submit</Text>
-        </TouchableOpacity>
-      </Animated.View>
-    </View>
-  </TouchableWithoutFeedback>
+        <Animated.View
+          style={[
+            styles.formContainer,
+            {
+              transform: [{translateY: formPosition}],
+              display: isFormVisible ? 'flex' : 'none',
+            },
+          ]}>
+          <Text style={styles.inputText}>Tracking Number</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="  Tracking Number"
+            placeholderTextColor="black"
+            value={formData.orderNum || ''}
+            onChangeText={text => setFormData({...formData, orderNum: text})}
+            onSubmitEditing={handleSubmit}
+            returnKeyType={'done'}
+            blurOnSubmit={true}
+          />
+          <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+            <Text style={styles.submitButtonText}>Submit</Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -270,26 +355,25 @@ const styles = StyleSheet.create({
   },
   submitButtonText: {
     fontSize: 15,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   inputText: {
     fontSize: 18,
     marginBottom: 10,
     fontWeight: 'bold',
-    marginLeft: 20,
+    marginLeft: 10,
+    color: '#000',
   },
   input: {
     borderWidth: 1,
     borderColor: '#000',
-    alignItems:'center',
-    alignSelf: 'auto',
     borderRadius: 20,
     backgroundColor: '#fff',
     paddingLeft: 20,
     paddingRight: 20,
     width: '80%',
     marginBottom: 20,
-    fontSize:16,
+    fontSize: 16,
   },
   formContainer: {
     padding: 10,
